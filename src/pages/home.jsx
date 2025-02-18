74188038259287fd2e8639a9370a48f1f443381a
Nestorfron <nestorfrones07@hotmail.com>
2025-02-18 09:58:31 -0300

    responsive

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -17,13 +17,15 @@ function Home() {
   }, []);
 
   return (
-    <div className="min-h-screen bg-backround">
+    <div className="min-h-screen bg-background">
       <main className="container mx-auto px-2 py-8">
         <div className="mb-8">
           <AdSpace position="top" />
         </div>
-        <div className="grid lg:grid-cols-3 gap-6">
-          <div className="lg:col-span-2 space-y-8 lg:space-y-0">
+
+        {/* Grid principal */}
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+          <div className="lg:col-span-2 space-y-8">
             {/* Partidos en Vivo */}
             <div className="max-w-4xl mx-auto p-4">
               <h1 className="text-2xl font-bold text-center mb-4">
@@ -63,7 +65,7 @@ function Home() {
             {/* Partidos Finalizados */}
             <div className="max-w-4xl mx-auto p-4">
               <h1 className="text-2xl font-bold text-center mb-4">
-                Resultados anterioes
+                Resultados anteriores
               </h1>
               <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-6 justify-center">
                 {store.pastMatches.length > 0 ? (
@@ -78,10 +80,12 @@ function Home() {
               </div>
             </div>
           </div>
-             {/* Equipos de la Temporada */}
-          <div className="mt-8">
+
+          {/* Barra lateral */}
+          <div className="lg:col-span-1 text-xs mt-8 lg:mt-0 space-y-8">
+
             {/* Equipos de la Temporada */}
-            <div className="max-w-lg mx-auto p-8 mt-8 lg:mt-0">
+            <div className="max-w-lg mx-auto p-8">
               <h1 className="text-2xl font-bold text-center mb-8">
                 Equipos de la Temporada
               </h1>
@@ -96,75 +100,76 @@ function Home() {
                   </p>
                 )}
               </div>
-              {/* Estadisticas*/}
-              <div className="mx-auto mt-8">
-                <h1 className="text-2xl font-bold text-center mb-8">
-                  Tabla de Posiciones
-                </h1>
-                {store.standings.length > 0 ? (
-                  <div className="overflow-x-auto bg-background rounded-lg shadow-xl border-t-4 border-primary-500">
-                    <table className="w-full text-center border-collapse">
-                      <thead>
-                        <tr className="bg-background/80">
-                          <th className="px-6 py-3 text-md">Pos</th>
-                          <th className="px-6 py-3 text-md">Equipo</th>
-                          <th className="px-6 py-3 text-md">Jugados</th>
-                          <th className="px-6 py-3 text-md">Ganados</th>
-                          <th className="px-6 py-3 text-md">Empatados</th>
-                          <th className="px-6 py-3 text-md">Perdidos</th>
-                          <th className="px-6 py-3 text-md">Dif. Goles</th>
-                          <th className="px-6 py-3 text-md">Pts</th>
+            </div>
+
+            {/* Estadísticas */}
+            <div className="max-w-lg mx-auto mt-8 p-2">
+              <h1 className="text-2xl font-bold text-center mb-8">
+                Tabla de Posiciones
+              </h1>
+              {store.standings.length > 0 ? (
+                <div className="overflow-x-auto bg-background rounded-lg shadow-xl border-t-4 border-primary-500">
+                  <table className="w-full text-center border-collapse">
+                    <thead>
+                      <tr className="bg-background/80">
+                        <th className="px-6 py-3 text-md">Pos</th>
+                        <th className="px-6 py-3 text-md">Equipo</th>
+                        <th className="px-6 py-3 text-md">Jugados</th>
+                        <th className="px-6 py-3 text-md">Ganados</th>
+                        <th className="px-6 py-3 text-md">Empatados</th>
+                        <th className="px-6 py-3 text-md">Perdidos</th>
+                        <th className="px-6 py-3 text-md">Dif. Goles</th>
+                        <th className="px-6 py-3 text-md">Pts</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {store.standings.map((standing, index) => (
+                        <tr
+                          key={index}
+                          className={`${
+                            index % 2 === 0
+                              ? "bg-background/10"
+                              : "bg-background/80"
+                          } transition-transform`}
+                        >
+                          <td className="px-6 py-3 border border-gray-700 dark:border-gray-600">
+                            {standing.rank}
+                          </td>
+                          <td className="px-6 py-3 border border-gray-700 dark:border-gray-600 font-medium">
+                            <img
+                              src={standing.team?.logo}
+                              alt="Bandera del equipo"
+                              className="w-6 h-6 m-auto"
+                            />
+                          </td>
+                          <td className="px-6 py-3 border border-gray-700 dark:border-gray-600">
+                            {standing.home.played + standing.away.played}
+                          </td>
+                          <td className="px-6 py-3 border border-gray-700 dark:border-gray-600">
+                            {standing.home.win + standing.away.win}
+                          </td>
+                          <td className="px-6 py-3 border border-gray-700 dark:border-gray-600">
+                            {standing.home.draw + standing.away.draw}
+                          </td>
+                          <td className="px-6 py-3 border border-gray-700 dark:border-gray-600">
+                            {standing.home.lose + standing.away.lose}
+                          </td>
+                          <td className="px-6 py-3 border border-gray-700 dark:border-gray-600">
+                            {standing.goalsDiff}
+                          </td>
+                          <td className="px-6 py-3 border border-gray-700 dark:border-gray-600">
+                            {standing.points}
+                          </td>
                         </tr>
-                      </thead>
-                      <tbody>
-                        {store.standings.map((standing, index) => (
-                          <tr
-                            key={index}
-                            className={`${
-                              index % 2 === 0
-                                ? "bg-background/10"
-                                : "bg-background/80"
-                            } transition-transform`}
-                          >
-                            <td className="px-6 py-3 border border-gray-700 dark:border-gray-600">
-                              {standing.rank}
-                            </td>
-                            <td className="px-6 py-3 border border-gray-700 dark:border-gray-600 font-medium">
-                              <img
-                                src={standing.team?.logo}
-                                alt="Bandera del equipo"
-                                className="w-6 h-6 m-auto"
-                              />
-                            </td>
-                            <td className="px-6 py-3 border border-gray-700 dark:border-gray-600">
-                              {standing.home.played + standing.away.played}
-                            </td>
-                            <td className="px-6 py-3 border border-gray-700 dark:border-gray-600">
-                              {standing.home.win + standing.away.win}
-                            </td>
-                            <td className="px-6 py-3 border border-gray-700 dark:border-gray-600">
-                              {standing.home.draw + standing.away.draw}
-                            </td>
-                            <td className="px-6 py-3 border border-gray-700 dark:border-gray-600">
-                              {standing.home.lose + standing.away.lose}
-                            </td>
-                            <td className="px-6 py-3 border border-gray-700 dark:border-gray-600">
-                              {standing.goalsDiff}
-                            </td>
-                            <td className="px-6 py-3 border border-gray-700 dark:border-gray-600">
-                              {standing.points}
-                            </td>
-                          </tr>
-                        ))}
-                      </tbody>
-                    </table>
-                  </div>
-                ) : (
-                  <p className="text-gray-400 text-center">
-                    Cargando estadísticas...
-                  </p>
-                )}
-              </div>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              ) : (
+                <p className="text-gray-400 text-center">
+                  Cargando estadísticas...
+                </p>
+              )}
             </div>
           </div>
         </div>
