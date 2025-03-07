49e4b7dd321007e26ee58c4c42c2579ed2b2d5a7
Nestorfron <nestorfrones07@hotmail.com>
2025-03-07 18:48:38 -0300

    live macthes whith sport-radar

--- src/store/flux.jsx
+++ src/store/flux.jsx
@@ -1,7 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
   return {
     store: {
-      matches: [],
+      liveMatches: [],
       finishedMatches: [],
       upcomingMatches: [],
       teams: [],
@@ -15,15 +15,13 @@ const getState = ({ getStore, getActions, setStore }) => {
         const URL = `https://api.sportradar.com/soccer/trial/v4/en/seasons/sr%3Aseason%3A128225/competitors.json?api_key=${API_KEY}`;
         const PROXY_URL = "https://corsproxy.io/?";
 
-
         try {
           const response = await fetch(PROXY_URL + URL, {
             headers: {
               accept: "application/json",
             },
           });
-          if (!response.ok)
-            throw new Error("Error al obtener los equipos");
+          if (!response.ok) throw new Error("Error al obtener los equipos");
 
           const data = await response.json();
           setStore({ teams: data.season_competitors });
@@ -47,62 +45,45 @@ const getState = ({ getStore, getActions, setStore }) => {
 
           const data = await response.json();
           setStore({ teamProfile: data });
-        }
-        catch (error) {
-          console.error("Error en getTeamProfile:", error);
-        }
-      },
-      getMatches: async () => {
-        const API_KEY = import.meta.env.VITE_API_KEY;
-        const LEAGUE_ID = 268;
-        const SEASON_YEAR = 2025;
-
-        try {
-          const response = await fetch(
-            `https://v3.football.api-sports.io/fixtures?league=${LEAGUE_ID}&season=${SEASON_YEAR}&live=all`,
-            {
-              headers: {
-                "x-apisports-key": API_KEY,
-              },
-            }
-          );
-
-          if (!response.ok) {
-            throw new Error("Error al obtener los partidos");
-          }
-
-          const data = await response.json();
-          setStore({ matches: data.response });
         } catch (error) {
-          console.error("Error en getMatches:", error);
+          console.error("Error en getTeamProfile:", error);
         }
       },
       getAllMatches: async () => {
         const API_KEY = import.meta.env.VITE_API_KEY2;
         const URL = `https://api.sportradar.com/soccer/trial/v4/en/seasons/sr%3Aseason%3A128225/summaries.json?api_key=${API_KEY}`;
         const PROXY_URL = "https://corsproxy.io/?";
-      
+
         try {
           const response = await fetch(PROXY_URL + URL, {
             headers: {
               accept: "application/json",
             },
           });
-      
+
           if (!response.ok) {
             throw new Error("Error al obtener los partidos");
           }
           const data = await response.json();
           if (data.summaries && Array.isArray(data.summaries)) {
-            const finishedMatches = data.summaries.filter(event => 
-              event.sport_event_status?.match_status === "ended" || event.sport_event_status?.status === "closed"
+            const finishedMatches = data.summaries.filter(
+              (event) =>
+                event.sport_event_status?.match_status === "ended" ||
+                event.sport_event_status?.status === "closed"
             );
-            const upcomingMatches = data.summaries.filter(event => 
-              event.sport_event_status?.match_status === "not_started" || event.sport_event_status?.status === "not_started"
+            const upcomingMatches = data.summaries.filter(
+              (event) =>
+                event.sport_event_status?.match_status === "not_started" ||
+                event.sport_event_status?.status === "not_started"
             );
-            setStore({ finishedMatches: finishedMatches, upcomingMatches: upcomingMatches });
+            setStore({
+              finishedMatches: finishedMatches,
+              upcomingMatches: upcomingMatches,
+            });
           } else {
-            console.error("No se encontraron partidos en la respuesta de la API.");
+            console.error(
+              "No se encontraron partidos en la respuesta de la API."
+            );
           }
         } catch (error) {
           console.error("Error en getFinishedMatches:", error);
@@ -114,7 +95,6 @@ const getState = ({ getStore, getActions, setStore }) => {
 
         const PROXY_URL = "https://corsproxy.io/?";
 
-
         try {
           const response = await fetch(PROXY_URL + URL, {
             headers: { accept: "application/json" },
@@ -124,7 +104,9 @@ const getState = ({ getStore, getActions, setStore }) => {
             throw new Error("Error al obtener la tabla de posiciones");
 
           const data = await response.json();
-          setStore({ standings2: data.season_form_standings[0].groups[0].form_standings });
+          setStore({
+            standings2: data.season_form_standings[0].groups[0].form_standings,
+          });
         } catch (error) {
           console.error("Error en getStandingsTable:", error);
         }
@@ -134,15 +116,13 @@ const getState = ({ getStore, getActions, setStore }) => {
         const URL = `https://api.sportradar.com/soccer/trial/v4/en/seasons/sr%3Aseason%3A128225/lineups.json?api_key=${API_KEY}`;
         const PROXY_URL = "https://corsproxy.io/?";
 
-
         try {
           const response = await fetch(PROXY_URL + URL, {
             headers: {
               accept: "application/json",
             },
           });
-          if (!response.ok)
-            throw new Error("Error al obtener los jugadores");
+          if (!response.ok) throw new Error("Error al obtener los jugadores");
 
           const data = await response.json();
           console.log(data.lineups);
@@ -155,15 +135,13 @@ const getState = ({ getStore, getActions, setStore }) => {
         const URL = `https://api.sportradar.com/soccer/trial/v4/en/seasons/sr%3Aseason%3A128225/leaders.json?api_key=${API_KEY}`;
         const PROXY_URL = "https://corsproxy.io/?";
 
-
         try {
           const response = await fetch(PROXY_URL + URL, {
             headers: {
               accept: "application/json",
             },
           });
-          if (!response.ok)
-            throw new Error("Error al obtener los líderes");
+          if (!response.ok) throw new Error("Error al obtener los líderes");
 
           const data = await response.json();
           setStore({ leaders: data.lists[1].leaders });
@@ -171,10 +149,52 @@ const getState = ({ getStore, getActions, setStore }) => {
           console.error("Error en getLeaders:", error);
         }
       },
+      getLiveMatchesInUruguay: async () => {
+        const API_KEY = import.meta.env.VITE_API_KEY2;
+        const URL = `https://api.sportradar.com/soccer/trial/v4/en/schedules/live/schedules.json?api_key=${API_KEY}`;
+        const PROXY_URL = "https://corsproxy.io/?";
+
+        try {
+          const response = await fetch(PROXY_URL + URL, {
+            headers: {
+              accept: "application/json",
+            },
+          });
+
+          if (!response.ok) {
+            throw new Error("Error al obtener los partidos");
+          }
+
+          const data = await response.json();
+
+          if (!data.schedules || !Array.isArray(data.schedules)) {
+            throw new Error("No se encontraron partidos en vivo.");
+          }
+
+          const partidosEnUruguay = data.schedules.filter((partido) => {
+            const categoria = partido.sport_event.sport_event_context?.category;
+            const status = partido.sport_event_status?.status;
+            const seasonIdFromResponse =
+              partido.sport_event.sport_event_context?.season?.id;
+
+            return (
+              categoria?.country_code === "URY" &&
+              status === "live" &&
+              seasonIdFromResponse === "sr:season:128225"
+            );
+          });
+
+          if (partidosEnUruguay.length === 0) {
+            throw new Error("No se encontraron partidos en vivo en Uruguay.");
+          }
+
+          setStore({ liveMatches: partidosEnUruguay });
+        } catch (error) {
+          console.error("Error en getLiveMatchesInUruguay:", error.message);
+        }
+      },
     },
   };
 };
 
 export default getState;
-
-
